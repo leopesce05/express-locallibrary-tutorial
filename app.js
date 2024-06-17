7044546a9545daed74beafc3591fa5311041d513
Leonardo Pesce <80352672+leopesce05@users.noreply.github.com>
2024-06-17 20:35:07 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,85 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const compression = require("compression");
+const helmet = require("helmet");
+const compression = require("compression");
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var catalogRouter = require('./routes/catalog'); 
+
+const { url } = require('inspector');
+
+var app = express();
+
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  }),
+);
+
+// Set up rate limiter: maximum of twenty requests per minute
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
+
+app.use(compression()); // Compress all routes
+
+//DATABASE SETUP
+const mongoose = require('mongoose');
+mongoose.set("strictQuery", false);
+
+
+const mongodb  = process.env.MONGODB_URI || 'mongodb+srv://libAPP:library@library.gt5eeu2.mongodb.net/?retryWrites=true&w=majority&appName=Library'
+
+//DATABASE CONNECTION
+main().catch(err => console.log(err));
+async function main() {
+  await mongoose.connect(mongodb);
+  console.log('Connected to MongoDB');
+}
+
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//RUTAS DE LA APLICACION
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
